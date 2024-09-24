4b4369d5f4af016ff337f5b7a5a588355a09bc7b
candi888 <cdi89724@gmail.com>
2024-09-24 15:00:34 +0900

    修正．yamlファイルの読み込みをinput_yaml/直下にあるyamlファイルすべてを自動で読み込むよう修正．回すときはmain_tmp.pyを実行すること

---
+++ main_tmp.py
@@ -0,0 +1,15 @@
+import subprocess
+from pathlib import Path
+
+
+def main() -> None:
+    input_yaml_dir_path = Path(__file__).parent / "input_yaml"
+    input_yaml_file_list = list(input_yaml_dir_path.glob("*.yaml"))
+
+    for input_yaml_file in input_yaml_file_list:
+        cmd = f"python plot.py {input_yaml_file.name}"
+        subprocess.run(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    main()
