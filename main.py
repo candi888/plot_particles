d27add02f6130660d9138eb9c860ca5c48689a6c
candi888 <cdi89724@gmail.com>
2024-12-14 23:36:23 +0900

    main.pyとplot_particles.pyの構造を修正

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import subprocess
-from pathlib import Path
+import plot_particles
 
 
 def main() -> None:
-    input_yaml_dir_path = Path(__file__).parent / "input_yaml"
-    input_yaml_file_path_list = list(input_yaml_dir_path.glob("*.yaml"))
-    input_yaml_file_path_list.sort()
-
-    for input_yaml_file_path in input_yaml_file_path_list:
-        cmd = (
-            f"python {str(Path(__file__).parent/"plot.py")} {input_yaml_file_path.name}"
-        )
-        subprocess.run(cmd, shell=True)
+    plot_particles.main_sub()
 
 
 if __name__ == "__main__":
